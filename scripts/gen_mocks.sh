268356472aa481c416a3a9ef999d98320e20680e
Kishor Joshi <joshikis@amazon.com>
2020-09-08 10:42:16 -0700

    Add VPC network subnet resolver (#1398)
    * add mocks for AWS EC2 service
    
    * add subnet resolver

--- scripts/gen_mocks.sh
+++ scripts/gen_mocks.sh
@@ -23,3 +23,4 @@ mockgen -destination=./mocks/controller-runtime/cache/mock.go sigs.k8s.io/contro
 mockgen -destination=./mocks/controller-runtime/controller/mock.go sigs.k8s.io/controller-runtime/pkg/controller Controller
 mockgen -destination=./mocks/controller-runtime/client/mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client
 mockgen -destination=./mocks/aws/services/mock_elbv2.go sigs.k8s.io/aws-alb-ingress-controller/pkg/aws/services ELBV2
+mockgen -destination=./mocks/aws/services/mock_ec2.go sigs.k8s.io/aws-alb-ingress-controller/pkg/aws/services EC2
