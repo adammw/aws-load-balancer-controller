2c0414608a7d1bdf95bd3af897859c1768f09ac1
M00nF1sh <yyyng@amazon.com>
2021-04-07 13:28:42 -0700

    add local zone support

--- scripts/gen_mocks.sh
+++ scripts/gen_mocks.sh
@@ -1,26 +1,5 @@
-#!/usr/bin/env bash
-# This shell script is used to generate & update mock objects for testing
-mockery -name CloudAPI -dir ./internal/aws/
-mockery -name Storer -dir ./internal/ingress/controller/store/ -inpkg
-
-mockery -name Controller -dir ./internal/alb/tags/ -inpkg
-mockery -name Controller -dir ./internal/alb/ls/ -inpkg
-mockery -name RulesController -dir ./internal/alb/ls/ -inpkg
-
-mockery -name ACMAPI -dir ./vendor/github.com/aws/aws-sdk-go/service/acm/acmiface
-mockery -name EC2API -dir ./vendor/github.com/aws/aws-sdk-go/service/ec2/ec2iface
-mockery -name ELBV2API -dir ./vendor/github.com/aws/aws-sdk-go/service/elbv2/elbv2iface
-mockery -name IAMAPI -dir ./vendor/github.com/aws/aws-sdk-go/service/iam/iamiface
-mockery -name ResourceGroupsTaggingAPIAPI -dir ./vendor/github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi/resourcegroupstaggingapiiface
-mockery -name WAFRegionalAPI -dir ./vendor/github.com/aws/aws-sdk-go/service/wafregional/wafregionaliface
-mockery -name WAFV2API -dir ./vendor/github.com/aws/aws-sdk-go/service/wafv2/wafv2iface
-mockery -name ShieldAPI -dir ./vendor/github.com/aws/aws-sdk-go/service/shield/shieldiface
-
-
-
-mockgen -destination=./mocks/aws-alb-ingress-controller/ingress/auth/mock.go github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/ingress/auth Module
-mockgen -destination=./mocks/controller-runtime/cache/mock.go sigs.k8s.io/controller-runtime/pkg/cache Cache
-mockgen -destination=./mocks/controller-runtime/controller/mock.go sigs.k8s.io/controller-runtime/pkg/controller Controller
+## Note: mocks for interfaces from this project should be along with the original package.
+##       mocks for interfaces from 3rd-party project should be put inside ./mocks folder.
 mockgen -destination=./mocks/controller-runtime/client/mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client
 mockgen -destination=./mocks/aws/services/mock_elbv2.go sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services ELBV2
 mockgen -destination=./mocks/aws/services/mock_ec2.go sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services EC2
@@ -30,4 +9,5 @@ mockgen -destination=./mocks/k8s/mock_finalizer.go sigs.k8s.io/aws-load-balancer
 mockgen -destination=./mocks/k8s/mock_repo_info_repo.go sigs.k8s.io/aws-load-balancer-controller/pkg/k8s PodInfoRepo
 mockgen -destination=./mocks/networking/mock_security_group_manager.go sigs.k8s.io/aws-load-balancer-controller/pkg/networking SecurityGroupManager
 mockgen -destination=./mocks/networking/mock_subnet_resolver.go sigs.k8s.io/aws-load-balancer-controller/pkg/networking SubnetsResolver
+mockgen -destination=./pkg/networking/az_info_provider_mocks.go sigs.k8s.io/aws-load-balancer-controller/pkg/networking AZInfoProvider
 mockgen -destination=./mocks/ingress/mock_cert_discovery.go sigs.k8s.io/aws-load-balancer-controller/pkg/ingress CertDiscovery
